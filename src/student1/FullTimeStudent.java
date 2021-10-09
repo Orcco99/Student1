abfd8a2ee26c9755137f66c72b77a3a330ea6b9b
Orcco99 <orccotom@sheridancollege.ca>
2021-10-08 23:44:29 -0400

    added FullTimeStudent class

---
+++ src/student1/FullTimeStudent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student1;
+
+/**
+ *
+ * @author oscar
+ */
+public class FullTimeStudent extends Student1 {
+    private int numSemesters;
+    
+    public int getNumSemesters(){
+            return numSemesters;
+    }
+    
+    public void setNumSemesters(int numSemesters){
+        this.numSemesters=numSemesters;
+    }
+    
+}
